fe1869e4177233d3361222e4a5c339e802e9142e
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-08 20:21:06 -0400

    solutions

---
+++ movezeroes.js
@@ -0,0 +1,22 @@
+//Objective is to move all zeroes in a 1-D array to the back in-place.
+
+let nums = [0,1,0,3,12]
+
+
+//O(n) solution that uses a slow pointer to keep track of where swaps happen.
+
+let curr = 0
+
+//Here, if we pass by a nonzero term, we swap it with the 'curr' pointer, which
+//initially starts at the beginning of the array and moves up one every time 
+//a swap occurs.
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+        let temp = nums[curr]
+        nums[curr++] = nums[i]
+        nums[i] = temp
+    }
+}
+
+return nums
