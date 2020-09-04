91019060d9ce1cf4d9e97b1591efcc02936df7ff
Kevin Dai <adamgordon@KevinDai.home>
2020-09-04 12:49:31 -0400

    added java solution

---
+++ movezeroes.java
@@ -0,0 +1,15 @@
+//Java Solution
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int curr = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[curr];
+                nums[curr++] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+}
